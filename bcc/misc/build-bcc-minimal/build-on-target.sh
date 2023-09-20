242d85dc1325839c529ffef9251729d40f82fedb
Daniel Edholm <daniel.edholm@zptcorp.com>
2023-09-20 17:37:38 +0200

    Small additional fixes

--- bcc/misc/build-bcc-minimal/build-on-target.sh
+++ bcc/misc/build-bcc-minimal/build-on-target.sh
@@ -3,6 +3,6 @@
 
 cd bcc-master
 rm -rf build && mkdir -p build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=./future-usr -DCMAKE_C_COMPILER=clang-7 -DCMAKE_CXX_COMPILER=clang++-7
+cmake .. -DCMAKE_INSTALL_PREFIX=./future-usr -DCMAKE_C_COMPILER=clang-11 -DCMAKE_CXX_COMPILER=clang++-11
 make -j90
 make install
