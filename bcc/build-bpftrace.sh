ec8e2a2747d51fa18330ef8146462cedb54a9261
Daniel Edholm <daniel.edholm@zptcorp.com>
2023-09-20 17:29:34 +0200

    Include option to use bpftrace

---
+++ bcc/build-bpftrace.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script should run within a bpftrace checkout
+
+spath=$(
+	cd "$(dirname "$0")"
+	pwd -P
+)
+cd $spath
+
+rm -rf build && mkdir -p build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_C_COMPILER=clang-11 -DCMAKE_CXX_COMPILER=clang++-11
+make -j$(nproc)
+make install
+rm -rf build
