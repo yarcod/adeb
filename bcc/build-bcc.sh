8fddf39b12f5655eac8b2f77a91772bb3e8ef87f
Daniel Edholm <daniel.edholm@zptcorp.com>
2023-09-20 17:29:54 +0200

    Use clang-11 to compile bcc as well

--- bcc/build-bcc.sh
+++ bcc/build-bcc.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # This script should run within a bcc checkout
 
-spath=$( cd "$(dirname "$0")" ; pwd -P )
+spath=$(
+	cd "$(dirname "$0")"
+	pwd -P
+)
 cd $spath
 
 rm -rf build && mkdir -p build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_C_COMPILER=clang-7 -DCMAKE_CXX_COMPILER=clang++-7
-make -j4
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_C_COMPILER=clang-11 -DCMAKE_CXX_COMPILER=clang++-11
+make -j$(nproc)
 make install
 cmake -DPYTHON_CMD=python3 ..
 pushd src/python/
